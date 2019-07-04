a07c9a307545c598fb87a6f085c87f29ed420a6b
joaodacolsoares <joao.dacol.soares@gmail.com>
2019-07-04 19:18:18 -0300

    Added the name dynamically

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-var getOurInformation = function () {
+ function getOurInformation() {
   const date1 = new Date();
   const date2 = new Date('12/24/2017');
   const timeDiff = Math.abs(date2.getTime() - date1.getTime());
   const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)) - 1;
 
   if ((diffDays) % 2 === 0) return 'Fran';
-  return 'João';
+  return 'John';
 }
 
-function setWhoLovesMore(e) {
-  console.log(e)
-  // document.getElementById('')
+function setWhoLovesMore() {
+  const titleId = 'person-who-loves-more'
+  document.getElementById(titleId).innerHTML = getOurInformation(); 
 }
