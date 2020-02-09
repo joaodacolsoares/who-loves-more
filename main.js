c9a45f4d1a51735aaccd74e230bbcf35e73f3c74
João Soares <41935580+joaodacolsoares@users.noreply.github.com>
2020-02-08 22:23:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
   const timeDiff = Math.abs(date2.getTime() - date1.getTime());
   const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)) - 1;
 
-  if ((diffDays) % 2 === 0) return 'Fran';
+  if ((diffDays) % 2 === 0) return 'João';
   return 'João';
 }
 
